d004736e39c70ab3a92a8ad98dfaa1f5f63ec8af
Thomas Klotz <t.klotz@student.uibk.ac.at>
2021-12-06 10:49:27 +0100

    setup layout

--- client/src/app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'bt5-root',
-  template: `<bt5-view></bt5-view>`,
-  styles: [],
-})
-export class AppComponent {}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { ViewComponent } from './view/view.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
-const MATERIAL_MODULES = [MatButtonModule];
+const MATERIAL_MODULES = [MatButtonModule, MatToolbarModule];
 
 @NgModule({
-  declarations: [AppComponent, ViewComponent],
+  declarations: [ViewComponent],
   imports: [BrowserModule, BrowserAnimationsModule, ...MATERIAL_MODULES],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [ViewComponent],
 })
 export class AppModule {}
